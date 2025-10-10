ad63e30a99f64a2a016cf1f24b84d7fa0e608062
dinoooop <dinoooop@gmail.com>
2025-10-10 12:19:45 +0530

    update for render

--- villa/settings.py
+++ villa/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Base url to serve media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
