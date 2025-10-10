c928b7a7b175bf759eec5dc9a793621f470a9d55
dinoooop <dinoooop@gmail.com>
2025-10-10 12:23:08 +0530

    minor update

--- villa/settings.py
+++ villa/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['dinoooop.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['villa-6bi4.onrender.com', 'dinoooop.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
